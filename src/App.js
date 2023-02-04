f46825ac2c897a85b45bbfffc626cf123683b8fd
shannonharrison118 <shannonharrison118@gmail.com>
2023-02-03 23:11:45 -0500

    basic layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+      <div className="App">
+        <header class="header">hiiii its me in digital form</header>
+        <div class="body">
+          <div class="box">
+            <div class="my-melody" data-url="me-and-melody.png">
+              <pre>   ⌒ </pre>
+              <pre> /        \   ⌒</pre>
+              <pre>|    |     /    ヽ</pre>
+              <pre> (⌒ｖ⌒)|/ |     |</pre>
+              <pre>  {'>'} ○ {'<'}    ヽ___/</pre>
+              <pre>  (＿人＿)　　   　＼</pre>
+              <pre> |　  ／￣￣￣＼    ｜</pre>
+              <pre> ヽ( ●   ｏ   ● )ﾉ</pre>
+              <pre>      ＞―亠―＜</pre>
+              <pre>      ＼_ノヽ_／ </pre>
+            </div>
+          </div>
+          <div class="box">
+            <p>me and my melody have the same birthday<br></br><small>(jan 18)</small></p>
+            <br></br>
+            <p>i have also gotten her on multiple "<i>which sanrio character are you?</i>" quizzes</p>
+          </div>
+          <div class="box">
+            <p>help! i put myself into the web</p>
+          </div>
+          <div class="box"><p>hi</p></div>
+          <div class="box"><p>testinggggg</p></div>
+          <div class="box"><p>seeing something</p></div>
+          <div class="box"><p>hmmm working good</p></div>
+      </div>
     </div>
   );
 }
